46bea35a1b828a4928b969d64fe7343b72402476
Kushagriisharma <en22cs304037@medicaps.ac.in>
2026-05-29 15:53:03 +0530

    Update SW cache version to v2 to force browser update

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables complete offline access in no-network environments.
  */
 
-const CACHE_NAME = 'alertify-cache-v1';
+const CACHE_NAME = 'alertify-cache-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
