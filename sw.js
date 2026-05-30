35774c8a742d48849799aa03ab8fea83ae8f40fc
Kushagriisharma <en22cs304037@medicaps.ac.in>
2026-05-30 12:39:28 +0530

    Restore background Twilio calling and SMS dispatches, removing native dialer tel redirect

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables complete offline access in no-network environments.
  */
 
-const CACHE_NAME = 'alertify-cache-v10';
+const CACHE_NAME = 'alertify-cache-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
